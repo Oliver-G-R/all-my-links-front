cb24a4096bca28570ce27e61c58708de56e08b14
Oliver Guerrero Ruiz <oliver.kali.hub@gmail.com>
2022-06-15 16:10:37 -0500

    add: update styles in profile page

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,38 +1,59 @@
 <script lang="ts" setup>
-    import { ref, computed, watch } from 'vue'
+    import { computed } from 'vue'
     import { useStore } from 'vuex'
     import { IState } from '../store/index'
     import { useGetUserByUrl } from '../composables/useGetUserByUrl'
 
     const store = useStore<IState>()
-
-    const isOwner = ref(false)
+    const userIdWithSession = computed(() => store.state.auth.user.id)
 
     const { user, loading } = useGetUserByUrl()
 
-    const userIdWithSession = computed(() => store.state.auth.user.id)
-
-    watch(() => user.value?.id, (newValue) => {
-        if (newValue === userIdWithSession.value) { isOwner.value = true }
+    const isOwner = computed(() => {
+        return userIdWithSession.value === user.value?.id
     })
 </script>
-<template>
-    <main class="container">
 
+<template>
+    <main>
         <p v-if="loading" > loading </p>
 
-        <section v-if="user && !loading">
-            <h1>Profile {{user.nickName}}</h1>
-            <router-link v-if="isOwner" to="/settings/profile">Editar perfil</router-link>
-            <ul>
-                <li v-for="link in user.links" :key="link.id">
-                    {{link.titleLink}} - <a target="_blank" :href="link.link">{{link.link}}</a>
-                </li>
-            </ul>
+        <section
+            class="section-profile"
+            v-if="user && !loading">
+           <div class="section-profile__container">
+                <div class="section-profile__content">
+                    <div class="section-profile__container-avatar">
+                        <img
+                            v-if="user.avatar_url"
+                            :src="user.avatar_url"
+                            alt="avatar">
+                        <span v-else> {{user.nickName[0]}} </span>
+                    </div>
+                    <h1>{{user.nickName}}</h1>
+                </div>
+
+                <div class="section-profile__content-btns">
+                    <button
+                        v-if="isOwner"
+                        class="section-profile__btn-profile section-profile__btn-profile--add" >
+                        +
+                    </button>
+                    <router-link
+                        v-if="isOwner"
+                        class="section-profile__btn-profile"
+                        to="/settings/profile">Update Profile</router-link>
+                    <button
+                        class="section-profile__btn-profile">
+                        Share profile
+                    </button>
+                </div>
+           </div>
         </section>
 
         <p v-else>
             No user found
         </p>
+
     </main>
 </template>
