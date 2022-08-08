31985c8b41d59fffa2d9c30b3124b50427a044ec
Oliver Guerrero Ruiz <oliver.kali.hub@gmail.com>
2022-08-08 10:18:33 -0500

    add: dev api and prod api in axios and config port

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,6 +1,10 @@
 import axios from 'axios'
-const urlApi = import.meta.env.VITE_API_URL
+const prodApi = import.meta.env.VITE_API_URL
+const productionMode = import.meta.env.PROD
 
+const urlApi = productionMode ? prodApi : 'http://localhost:3000/all-my-links-api/v1'
+
+console.log('urlApi', urlApi)
 export const linksApi = axios.create({
     baseURL: urlApi
 })
